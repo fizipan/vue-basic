2a2fadb7ee72acbd47d1e75f4887fe38031b251f
hafizhmaulanay <56567633+hafizhmaulanay@users.noreply.github.com>
2022-08-17 15:04:17 +0700

    list-rendering

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,33 @@
 <template>
-  <h2 v-if="num === 0">The Number is Zero</h2>
-  <h2 v-else-if="num < 0">The number is negative</h2>
-  <h2 v-else-if="num > 0">The number is positive</h2>
-  <h2 v-else>Not a number</h2>
-
-  <div v-if="display">
-    <h2>Hafizh</h2>
-    <h2>WP 17</h2>
-    <h2>Vue js</h2>
-  </div>
-
+  <h2>List Names</h2>
+  <ul>
+    <li v-for="(name, index) in names" :key="index">{{ index + 1 }}. {{ name }}</li>
+  </ul>
   <hr />
-
-  <div v-show="showElement">
-    <h2>Hafizh</h2>
-    <h2>WP 17</h2>
-    <h2>Vue js</h2>
-  </div>
+  <h2>List Fullnames</h2>
+  <ul>
+    <li v-for="fullname in fullNames" :key="fullname.firstName">
+      {{ fullname.firstName }} {{ fullname.lastName }}
+    </li>
+  </ul>
+  <hr />
+  <h2>List Actors</h2>
+  <ul>
+    <li v-for="actor in actors" :key="actor.name">
+      {{ actor.name }}
+      <ul>
+        <li v-for="movie in actor.movies" :key="movie">
+          {{ movie }}
+        </li>
+      </ul>
+    </li>
+  </ul>
+  <h2>List Actors</h2>
+  <ul>
+    <li v-for="(info, key, index) in myInfo" :key="info.name">
+      {{ index + 1 }}. {{ key }}: {{ info }}
+    </li>
+  </ul>
 </template>
 
 <script>
@@ -24,9 +35,40 @@ export default {
   name: "App",
   data() {
     return {
-      num: "hahha",
-      display: true,
-      showElement: false,
+      names: ["John", "Jane", "Joe"],
+      fullNames: [
+        {
+          firstName: "Bruce",
+          lastName: "Wayne",
+        },
+        {
+          firstName: "Clark",
+          lastName: "Kent",
+        },
+        {
+          firstName: "Midun",
+          lastName: "Doe",
+        },
+      ],
+      actors: [
+        {
+          name: "Hafizh",
+          movies: ["The Avengers", "The Dark Knight", "The Dark Knight Rises"],
+        },
+        {
+          name: "Haikal",
+          movies: ["Spiderman", "Catwowan"],
+        },
+        {
+          name: "Midun",
+          movies: ["Pinokio", "Barudak Teuing"],
+        },
+      ],
+      myInfo: {
+        name: "Hafizh",
+        age: 20,
+        address: "Jl. Raya",
+      },
     }
   },
 }
