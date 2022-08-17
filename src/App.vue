51ec974db0128dc4355dc16c032b36ef5abe409f
hafizhmaulanay <56567633+hafizhmaulanay@users.noreply.github.com>
2022-08-17 14:38:40 +0700

    contional-rendering

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,22 @@
 <template>
-  <h2 :id="headingId">Heading</h2>
-  <button :disabled="isDisabled">Disabled</button>
-  <h2 class="underline">Underline Text</h2>
-  <h2 :class="status">Status</h2>
-  <h2 :class="isPromoted && 'promoted'">Promoted Movie</h2>
-  <h2 :class="isSoldOut ? 'sold-out' : 'new'">New / Sold Out</h2>
-  <h2 :class="['new', 'promoted']">Newly promoted movie</h2>
-  <h2 :class="[isPromoted && 'promoted', isSoldOut ? 'sold-out' : 'new']">
-    Array conditional movie
-  </h2>
-  <h2 :class="{ promoted: isPromoted, new: !isSoldOut, 'sold-out': isSoldOut }">
-    Object Conditional Movie
-  </h2>
+  <h2 v-if="num === 0">The Number is Zero</h2>
+  <h2 v-else-if="num < 0">The number is negative</h2>
+  <h2 v-else-if="num > 0">The number is positive</h2>
+  <h2 v-else>Not a number</h2>
 
-  <h2 :style="{ color: highlighColor, fontSize: headerSize + 'px' }">Highlighted Text</h2>
-  <h2 :style="[baseStyle, successStyle]">Array Highlighted Text</h2>
-  <h2 :style="[baseStyle, dangerStyle]">Array Highlighted Text</h2>
+  <div v-if="display">
+    <h2>Hafizh</h2>
+    <h2>WP 17</h2>
+    <h2>Vue js</h2>
+  </div>
+
+  <hr />
+
+  <div v-show="showElement">
+    <h2>Hafizh</h2>
+    <h2>WP 17</h2>
+    <h2>Vue js</h2>
+  </div>
 </template>
 
 <script>
@@ -23,22 +24,9 @@ export default {
   name: "App",
   data() {
     return {
-      headingId: "heading-id",
-      isDisabled: true,
-      status: "danger",
-      isPromoted: false,
-      isSoldOut: true,
-      highlighColor: "orange",
-      headerSize: "50",
-      baseStyle: {
-        fontSize: "20px",
-      },
-      successStyle: {
-        backgroundColor: "green",
-      },
-      dangerStyle: {
-        backgroundColor: "red",
-      },
+      num: "hahha",
+      display: true,
+      showElement: false,
     }
   },
 }
